cc354e97d133077d40f5cbcd7544a8ea170f11a0
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-03-29 14:04:59 +0100

    done using arrays and its sort method

---
+++ Solution.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+
+        Arrays.sort(sChar);
+        Arrays.sort(tChar);
+
+        return Arrays.equals(sChar, tChar);
+
+    }
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.isAnagram("cat", "tac"));
+    }
+}
